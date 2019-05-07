f4f67963342a74f83ac78585de37ac1f12428dff
n0an <nantog@gmail.com>
2019-05-07 23:19:53 +0300

    init

---
+++ BitPrice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BitPrice
+//
+//  Created by nag on 07/05/2019.
+//  Copyright © 2019 Anton Novoselov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
