139ff666687e30c94a7fb3643631f008dcd54ec6
n0an <nantog@gmail.com>
2019-05-09 23:25:27 +0300

    cleanup

--- BitPrice WatchKit Extension/InterfaceController.swift
+++ BitPrice WatchKit Extension/InterfaceController.swift
@@ -15,7 +15,7 @@ class InterfaceController: WKInterfaceController {
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
         
-        // Configure interface objects here.
+        
     }
     
     override func willActivate() {
